2e6597d5e4cc3c5d31b8f2f6ac5c266c25d92473
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2023-10-18 00:12:05 +0530

    New changes

---
+++ scriptjs.js
@@ -0,0 +1,7 @@
+function display(){
+    var username=document.getElementById('uname').value;
+    var password=document.getElementById('pass').value;
+    if(username === "vits" && password === "vits2023"){
+        window.open("index2.html");
+    }
+}
